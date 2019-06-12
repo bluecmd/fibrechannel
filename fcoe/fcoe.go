c80d6dc0234a62e88388eb8075e2746c31bc47d0
Christian Svensson <blue@cmd.nu>
2019-06-12 22:03:59 +0200

    FCoE marshaling and CRC32

--- fcoe/fcoe.go
+++ fcoe/fcoe.go
@@ -2,6 +2,7 @@ package fcoe
 
 import (
 	"encoding/binary"
+	"hash/crc32"
 	"io"
 
 	fc "github.com/bluecmd/fibrechannel"
@@ -47,7 +48,7 @@ type Frame struct {
 
 func (f *Frame) UnmarshalBinary(b []byte) error {
 	var ok bool
-	if len(b) < 18 {
+	if len(b) < 22 {
 		return io.ErrUnexpectedEOF
 	}
 
@@ -62,6 +63,52 @@ func (f *Frame) UnmarshalBinary(b []byte) error {
 		return fc.ErrInvalidEOF
 	}
 	f.CRC32 = binary.BigEndian.Uint32(b[len(b)-8 : len(b)-4])
+	return nil
+}
+
+func (f *Frame) Checksum() uint32 {
+	h := crc32.ChecksumIEEE(f.Payload)
+	// Change from big-endian to host encoding
+	b := [4]byte{
+		uint8(h >> 0),
+		uint8(h >> 8),
+		uint8(h >> 16),
+		uint8(h >> 24),
+	}
+	return binary.BigEndian.Uint32(b[:])
+}
+
+func (f *Frame) MarshalBinary() ([]byte, error) {
+	b := make([]byte, f.length())
+	err := f.read(b)
+	return b, err
+}
+
+func (f *Frame) read(b []byte) error {
+	// Version
+	b[0] = byte(f.Version)
+
+	// TODO(bluecmd): This can be made faster, but the map is so small so
+	// should be fine
+	for k, v := range sofMap {
+		if v == f.SOF {
+			b[13] = k
+			break
+		}
+	}
+	copy(b[14:], f.Payload)
+
+	binary.BigEndian.PutUint32(b[len(b)-8:len(b)-4], f.CRC32)
+	for k, v := range eofMap {
+		if v == f.EOF {
+			b[len(b)-4] = k
+			break
+		}
+	}
 
 	return nil
+}
+
+func (f *Frame) length() int {
+	return len(f.Payload) + 22
 }
--- fcoe/fcoe_test.go
+++ fcoe/fcoe_test.go
@@ -9,6 +9,37 @@ import (
 	fc "github.com/bluecmd/fibrechannel"
 )
 
+func TestFrameMarshalBinary(t *testing.T) {
+	var tests = []struct {
+		desc string
+		f    *Frame
+		b    []byte
+		err  error
+	}{
+		{
+			desc: "Normal frame",
+			f:    &Frame{SOF: fc.SOFf, EOF: fc.EOFn, CRC32: 0x10},
+			b:    append(bytes.Repeat([]byte{0}, 13), 0x28, 0, 0, 0, 0x10, 0x41, 0, 0, 0),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			b, err := tt.f.MarshalBinary()
+			if err != nil {
+				if want, got := tt.err, err; want != got {
+					t.Fatalf("unexpected error: %v != %v", want, got)
+				}
+				return
+			}
+
+			if want, got := tt.b, b; !bytes.Equal(want, got) {
+				t.Fatalf("unexpected Frame bytes:\n- want: %v\n-  got: %v", want, got)
+			}
+		})
+	}
+}
+
 func TestFrameUnmarshalBinary(t *testing.T) {
 	var tests = []struct {
 		desc string
@@ -58,5 +89,19 @@ func TestFrameUnmarshalBinary(t *testing.T) {
 				t.Fatalf("unexpected Frame:\n- want: %v\n-  got: %v", want, got)
 			}
 		})
+	}
+}
+
+func TestCRC32(t *testing.T) {
+	p := []byte{
+		0x22, 0xff, 0xff, 0xfd, 0x00, 0xed, 0x01, 0x00, 0x01, 0x29, 0x00, 0x00,
+		0xf0, 0x00, 0x00, 0x00, 0x03, 0xf8, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00,
+		0x62, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03}
+	f := &Frame{Payload: p}
+	f.CRC32 = f.Checksum()
+	want := uint32(0x1b726f69)
+	got := f.CRC32
+	if want != got {
+		t.Fatalf("CRC calculation failed: wanted %08x, got %08x", want, got)
 	}
 }
